11aa3e7008dfd3fa70ea36d0ed459e69e39c525a
Aaron-cdx <1315020626@qq.com>
2020-01-01 22:07:33 +0800

    排序-二分插入排序 添加详细解释

--- src/main/java/com/duanxi/sortingAlgorithm/BinaryInsertionSort.java
+++ src/main/java/com/duanxi/sortingAlgorithm/BinaryInsertionSort.java
@@ -39,6 +39,43 @@ public class BinaryInsertionSort {
         }
     }
 
+    /**
+     * 详细解释版本
+     * 这里是通过每次插入第i个元素的时候，比较第i个元素与之前的元素中间元素对比，通过二分法。
+     * 如果小的话表名在之前插入，如果大的话，表名在之后插入。直到low>high为止，然后数组移位元素
+     * 将当前i记录，数组移位，然后填充即可
+     *
+     * @param nums
+     */
+    public static void binaryInsertion(int[] nums) {
+        // 二分插入法-每次折半查找
+        for (int i = 1; i < nums.length; i++) {
+            int temp = nums[i];
+            // temp就是当前元素，low high主要是为了定位插入的位置
+            int low = 0;
+            int high = i - 1;
+            int mid = 0;
+            while (low <= high) {
+                mid = low + ((high - low) / 2);
+                // 注意比较的是mid
+                if (nums[mid] > temp) {
+                    // 如果大于的话，表名肯定是在low和hign的前半段
+                    high = mid - 1;
+                } else {
+                    // 否则的话在后半段
+                    low = mid + 1;
+                }
+            }
+            // 如果最后low超过了high，也有可能是high自己递减小于了low,此时停止
+            // 表明找到了要插入的位置,最后的话一定是low位置找到了，也就是最小的位置插入当前元素
+            for (int j = i - 1; j >= low; j--) {
+                nums[j + 1] = nums[j];
+            }
+            // low表示那个位置，表名low之后的元素都需要移动
+            nums[low] = temp;
+        }
+    }
+
     public static void swap(int[] nums, int a, int b) {
         int temp = nums[a];
         nums[a] = nums[b];
